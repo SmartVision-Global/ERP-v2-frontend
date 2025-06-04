4bb27ad6a60b5e15c0fdb34c66b0670ee0396e88
Rabah-Sebti <sebtirabah19@gmail.com>
2025-06-04 11:41:32 +0100

    add and remove element from payroll

--- src/sections/r-h/payroll-management/calculation/calculation-salary-grid-table-body.jsx
+++ src/sections/r-h/payroll-management/calculation/calculation-salary-grid-table-body.jsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
+import { useBoolean } from 'minimal-shared/hooks';
+
 import {
   Stack,
+  Button,
   Tooltip,
   TableRow,
   TableBody,
@@ -15,6 +19,7 @@ import { showError } from 'src/utils/toast-error';
 import { createPersonalPayroll } from 'src/actions/payroll-calculation';
 
 import { Iconify } from 'src/components/iconify';
+import { ConfirmDialog } from 'src/components/custom-dialog';
 
 export function CalculationSalaryGridTableBody({
   payroll,
@@ -25,6 +30,9 @@ export function CalculationSalaryGridTableBody({
   setValue,
   setPayroll,
 }) {
+  const [elementId, setElementId] = useState();
+  const confirmDialog = useBoolean();
+
   // const cotis_impos = fields.filter((item) => item.contributory_imposable === 1);
   // const no_cotis_impos = fields.filter((item) => item.contributory_imposable === 2);
   // const no_cotis_no_impos = fields.filter((item) => item.contributory_imposable === 3);
@@ -48,6 +56,7 @@ export function CalculationSalaryGridTableBody({
     try {
       const response = await createPersonalPayroll(id, dataPayroll);
       setPayroll(response.data?.data);
+      setValue('elements', []);
       console.log('respo', response);
     } catch (error) {
       console.log(error);
@@ -55,153 +64,194 @@ export function CalculationSalaryGridTableBody({
     }
   };
 
-  return (
-    <TableBody>
-      {newElements?.map((element) => (
-        <TableRow
-          key={element.name}
-          sx={{
-            backgroundColor:
-              element.name === 'Salaire de poste' ||
-              element.name === '9 % Sécuritê sociale' ||
-              element.name === 'Salaire Imposable' ||
-              element.name === 'IRG Mensuel'
-                ? 'success.lighter'
-                : 'inherit',
+  const handleRemoveElement = async () => {
+    const dataPayroll = {
+      additional_elements: [],
+      removed_elements: [elementId],
+    };
+    try {
+      const response = await createPersonalPayroll(payroll.id, dataPayroll);
+      setPayroll(response.data?.data);
+      console.log('respo', response);
+    } catch (error) {
+      console.log(error);
+      showError(error);
+    }
+  };
+  const renderConfirmDialog = () => (
+    <ConfirmDialog
+      open={confirmDialog.value}
+      onClose={confirmDialog.onFalse}
+      title="Supprimer"
+      content={<>Êtes-vous sûr de vouloir effacer ?</>}
+      action={
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => {
+            // handleDeleteRows();
+            handleRemoveElement();
+            confirmDialog.onFalse();
           }}
         >
-          <TableCell>
-            <Typography variant="body2">{element.type}</Typography>
-          </TableCell>
+          Supprimer
+        </Button>
+      }
+    />
+  );
+  return (
+    <>
+      <TableBody>
+        {newElements?.map((element) => (
+          <TableRow
+            key={element.name}
+            sx={{
+              backgroundColor:
+                element.name === 'Salaire de poste' ||
+                element.name === '9 % Sécuritê sociale' ||
+                element.name === 'Salaire Imposable' ||
+                element.name === 'IRG Mensuel'
+                  ? 'success.lighter'
+                  : 'inherit',
+            }}
+          >
+            <TableCell>
+              <Typography variant="body2">{element.type}</Typography>
+            </TableCell>
 
-          <TableCell>
-            <Typography variant="body2" fontWeight={500}>
-              {element.name}
-            </Typography>
-          </TableCell>
-          <TableCell>
-            <Typography variant="body2" fontWeight={500}>
-              {element.source === 'form' ? newElements[0].amount : element.day_base}
-              {/* {element.day_base} */}
-            </Typography>
-          </TableCell>
-          <TableCell>
-            {element.source === 'form' ? (
-              0 // <TextField {...register(`elements.${element.formIndex}.amount`)} size="small" />
-            ) : (
+            <TableCell>
               <Typography variant="body2" fontWeight={500}>
-                {fNumber(element.amount)}
+                {element.name}
               </Typography>
-            )}
+            </TableCell>
+            <TableCell>
+              <Typography variant="body2" fontWeight={500}>
+                {element.source === 'form' ? newElements[0].amount : element.day_base}
+                {/* {element.day_base} */}
+              </Typography>
+            </TableCell>
+            <TableCell>
+              {element.source === 'form' ? (
+                0 // <TextField {...register(`elements.${element.formIndex}.amount`)} size="small" />
+              ) : (
+                <Typography variant="body2" fontWeight={500}>
+                  {fNumber(element.amount)}
+                </Typography>
+              )}
 
-            {/* <Typography variant="body2" fontWeight={500}>
+              {/* <Typography variant="body2" fontWeight={500}>
               {fNumber(element.amount)}
             </Typography> */}
-          </TableCell>
-          <TableCell>
-            {/* <Typography variant="body2" fontWeight={500}>
+            </TableCell>
+            <TableCell>
+              {/* <Typography variant="body2" fontWeight={500}>
               0
             </Typography> */}
-            {element.source === 'form' ? (
-              <TextField
-                {...register(`elements.${element.formIndex}.percent`)}
-                onChange={(e) => {
-                  const newPercent = parseFloat(e.target.value);
-                  if (isNaN(newPercent)) return;
+              {element.source === 'form' ? (
+                <TextField
+                  {...register(`elements.${element.formIndex}.percent`)}
+                  onChange={(e) => {
+                    const newPercent = parseFloat(e.target.value);
+                    if (isNaN(newPercent)) return;
 
-                  const newAmount = (newPercent * newElements[0]?.salary_base) / 100;
+                    const newAmount = (newPercent * newElements[0]?.salary_base) / 100;
 
-                  update(element.formIndex, {
-                    ...fields[element.formIndex],
-                    tax: newPercent,
-                    amount: Number(newAmount.toFixed(2)),
-                  });
-                  setValue(`elements.${element.formIndex}.amount`, Number(newAmount.toFixed(2)));
-                }}
-                size="small"
-                type="number"
-              />
-            ) : (
+                    update(element.formIndex, {
+                      ...fields[element.formIndex],
+                      tax: newPercent,
+                      amount: Number(newAmount.toFixed(2)),
+                    });
+                    setValue(`elements.${element.formIndex}.amount`, Number(newAmount.toFixed(2)));
+                  }}
+                  size="small"
+                  type="number"
+                />
+              ) : (
+                <Typography variant="body2" fontWeight={500}>
+                  0
+                </Typography>
+              )}
+            </TableCell>
+            <TableCell>
               <Typography variant="body2" fontWeight={500}>
-                0
+                {/* {element.tax} */}0
               </Typography>
-            )}
-          </TableCell>
-          <TableCell>
-            <Typography variant="body2" fontWeight={500}>
-              {/* {element.tax} */}0
-            </Typography>
-            {/* {element.source === 'form' ? (
+              {/* {element.source === 'form' ? (
               <TextField {...register(`elements.${element.formIndex}.percent`)} size="small" />
             ) : (
               <Typography variant="body2" fontWeight={500}>
                 {fNumber(element.tax)}
               </Typography>
             )} */}
-          </TableCell>
-          <TableCell>
-            {/* <Typography variant="body2" fontWeight={500}>
+            </TableCell>
+            <TableCell>
+              {/* <Typography variant="body2" fontWeight={500}>
               {fNumber(element.salary_base)}
             </Typography> */}
-            {element.source === 'form' ? (
-              <TextField
-                type="number"
-                {...register(`elements.${element.formIndex}.amount`)}
-                size="small"
-                onChange={(e) => {
-                  const newAmount = parseFloat(e.target.value);
-                  if (isNaN(newAmount)) return;
+              {element.source === 'form' ? (
+                <TextField
+                  type="number"
+                  {...register(`elements.${element.formIndex}.amount`)}
+                  size="small"
+                  onChange={(e) => {
+                    const newAmount = parseFloat(e.target.value);
+                    if (isNaN(newAmount)) return;
 
-                  const newTax = (newAmount * 100) / newElements[0]?.salary_base;
-                  console.log(newTax);
+                    const newTax = (newAmount * 100) / newElements[0]?.salary_base;
+                    console.log(newTax);
 
-                  update(element.formIndex, {
-                    ...fields[element.formIndex],
-                    amount: newAmount,
-                    percent: Number(newTax.toFixed(2)),
-                  });
-                  setValue(`elements.${element.formIndex}.percent`, Number(newTax.toFixed(2)));
-                }}
-              />
-            ) : (
-              <Typography variant="body2" fontWeight={500}>
-                {/* {fNumber(element.tax)} */}
-                {fNumber(element.salary_base)}
-              </Typography>
-            )}
-          </TableCell>
-          {element.source === 'form' ? (
-            <TableCell>
-              <Stack direction="row" spacing={1}>
-                <Tooltip title="Valider">
-                  <IconButton onClick={() => handleCalculatePayroll(payroll.id, element)}>
-                    <Iconify icon="eva:checkmark-fill" sx={{ color: 'success.main' }} />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="Retirer">
-                  <IconButton onClick={() => remove(element.formIndex)}>
-                    <Iconify icon="eva:trash-2-fill" sx={{ color: 'error.main' }} />
-                  </IconButton>
-                </Tooltip>
-              </Stack>
+                    update(element.formIndex, {
+                      ...fields[element.formIndex],
+                      amount: newAmount,
+                      percent: Number(newTax.toFixed(2)),
+                    });
+                    setValue(`elements.${element.formIndex}.percent`, Number(newTax.toFixed(2)));
+                  }}
+                />
+              ) : (
+                <Typography variant="body2" fontWeight={500}>
+                  {fNumber(element.salary_base)}
+                </Typography>
+              )}
             </TableCell>
-          ) : (
-            <>
-              {element.is_deletable && (
-                <TableCell>
+            {element.source === 'form' ? (
+              <TableCell>
+                <Stack direction="row" spacing={1}>
+                  <Tooltip title="Valider">
+                    <IconButton onClick={() => handleCalculatePayroll(payroll.id, element)}>
+                      <Iconify icon="eva:checkmark-fill" sx={{ color: 'success.main' }} />
+                    </IconButton>
+                  </Tooltip>
                   <Tooltip title="Retirer">
-                    <IconButton onClick={() => console.log('retirer')}>
-                      {/* <Iconify icon="eva:person-done-fill" sx={{ color: 'success.main' }} /> */}
+                    <IconButton onClick={() => remove(element.formIndex)}>
                       <Iconify icon="eva:trash-2-fill" sx={{ color: 'error.main' }} />
                     </IconButton>
                   </Tooltip>
-                </TableCell>
-              )}
-            </>
-          )}
-        </TableRow>
-      ))}
-      {/* <TableRow>
+                </Stack>
+              </TableCell>
+            ) : (
+              <>
+                {element.is_deletable && (
+                  <TableCell>
+                    <Tooltip title="Retirer">
+                      <IconButton
+                        // onClick={() => handleRemoveElement(element.deduction_compensation_id)}
+                        onClick={() => {
+                          setElementId(element.deduction_compensation_id);
+                          confirmDialog.onTrue();
+                        }}
+                      >
+                        {/* <Iconify icon="eva:person-done-fill" sx={{ color: 'success.main' }} /> */}
+                        <Iconify icon="eva:trash-2-fill" sx={{ color: 'error.main' }} />
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                )}
+              </>
+            )}
+          </TableRow>
+        ))}
+        {/* <TableRow>
         <TableCell>
           <Typography>0</Typography>
         </TableCell>
@@ -441,7 +491,9 @@ export function CalculationSalaryGridTableBody({
           <Typography>53,402.40</Typography>
         </TableCell>
       </TableRow> */}
-    </TableBody>
+      </TableBody>
+      {confirmDialog.value && elementId && renderConfirmDialog()}
+    </>
   );
 }
 
